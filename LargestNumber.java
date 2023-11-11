52d4054e4aaba0c985d004842d17ceb7cd2a5667
johrielrsrd <johrielrsrd@gmail.com>
2023-11-11 12:29:51 +0800

    updated arraylist

--- LargestNumber.java
+++ LargestNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LargestNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> arrayNum = new ArrayList<>(3);
+        ArrayList<Integer> arrayNum = new ArrayList<>();
 
         for (int i = 1; i <= 3; i++) {
             System.out.print("Please enter INPUT" + i + " : ");
